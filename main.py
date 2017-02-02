b83cbcb8b6063c80bb5bbfdb59a7c909aaaa8f6a
Hunter Kinder <markhunterkinder@gmail.com>
2017-02-02 08:12:21 -0600

    coded for lucky number, fortune cookie and button

--- main.py
+++ main.py
@@ -15,10 +15,37 @@
 # limitations under the License.
 #
 import webapp2
+import random
 
+def getRandomFortune():
+	fortunes = [
+	"HTML and CSS are to Daoism, as Python is to Confucianism",
+	"Much activity is to be expected on your motherboard",
+	"The root of your success lies between your fingertips and your keyboard." ]
+	
+	index = random.randint(0,2)
+	
+	return fortunes[index]
+	
+	
+	
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+    def get(self): 	
+    	header = "<h1> Fortune Cookie </h1>"
+    	
+    	fortune = getRandomFortune()
+    	fortune_sentence = "Your fortune is: " + "<strong>" + fortune + "</strong>"
+    	fortune_paragraph = "<p>" + fortune_sentence + "</p>" 
+    	
+    	lucky_number = random.randint(1,100)
+    	number_sentence = "Your lucky number is: " + "<strong>" + str(lucky_number) + "</strong>"
+    	number_paragraph = "<p>" + number_sentence + "</p>"
+    	
+    	another_cookie = "<a href='.'><button> Another cookie, plz! </button></a>"
+    	
+    	content = header + number_paragraph + fortune_paragraph + another_cookie
+    	
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
